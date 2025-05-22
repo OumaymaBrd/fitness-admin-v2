f03d6145a4148e8058ba0d3d75bbd280e44b7361
OumaymaBrd <oumaymabramid@gmail.com>
2025-05-22 09:54:19 +0100

    Ajouter le controller de ProfileController

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display the user's profile form.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit()
+    {
+        return view('admin.profile', [
+            'user' => Auth::user(),
+        ]);
+    }
+
+    /**
+     * Update the user's profile information.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $user->id],
+        ]);
+
+        $user->fill($validated);
+        $user->save();
+
+        return redirect()->route('profile.edit')->with('status', 'profile-updated');
+    }
+
+    /**
+     * Delete the user's account.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(Request $request)
+    {
+        $request->validate([
+            'password' => ['required', 'current-password'],
+        ]);
+
+        $user = Auth::user();
+
+        Auth::logout();
+
+        $user->delete();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
